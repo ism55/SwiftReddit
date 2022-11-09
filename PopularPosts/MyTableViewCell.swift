ca11d1e998ad2432c1186074d46a0d0346bfbdba
ism55 <ismael_1_55@hotmail.com>
2022-11-09 19:34:09 -0400

    Version Ready

--- PopularPosts/MyTableViewCell.swift
+++ PopularPosts/MyTableViewCell.swift
@@ -10,6 +10,15 @@ import SDWebImage
 
 
 class MyTableViewCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subredditLabel: UILabel!
+    @IBOutlet var authorLabel: UILabel!
+    @IBOutlet var commentsLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var thumbnailView: UIImageView!
+    @IBOutlet var containerView: UIView!
+    
     
     static let identifier = "MyTableViewCell"
     
@@ -17,62 +26,51 @@ class MyTableViewCell: UITableViewCell {
         return UINib(nibName: "MyTableViewCell", bundle: nil)
     }
     
-    
-    
-    public func configure (with title:String, subreddit:String, author:String, comments:Int, date:TimeInterval, thumbnailURL: URL?){
+    public func configure (with title:String, subreddit:String, author:String, comments:Int, date:TimeInterval, thumbnailURL: URL?, auxURL: URL?){
         let parseDate = Date(timeIntervalSince1970: date)
         let dateFormatter = DateFormatter()
         dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
         dateFormatter.locale = NSLocale.current
         dateFormatter.dateFormat = "MM/dd/yyyy"
         let strDate = dateFormatter.string(from: parseDate)
-        
-        
-        
+
         titleLabel.text = title
         authorLabel.text = "\("posted_by"~)" + " u/" + author
         subredditLabel.text = subreddit
         commentsLabel.text = "💬" + comments.roundedWithAbbreviations + " " + "\("comments_str"~)"
         dateLabel.text = strDate
-        if thumbnailURL!.absoluteString != "default" {
-            thumbnailView.sd_setImage(with: thumbnailURL)
-        }else{
+        if thumbnailURL!.absoluteString == "default" {
             thumbnailView.removeFromSuperview()
+            // TO DO: Click Gesture
+        }else if thumbnailURL!.absoluteString == "image"{
+            thumbnailView.sd_setImage(with: auxURL)
+        }else{
+            thumbnailView.sd_setImage(with: thumbnailURL)
         }
-        
         titleLabel.textColor = UIColor.black
         subredditLabel.adjustsFontSizeToFitWidth = false
         subredditLabel.lineBreakMode = .byTruncatingTail
         authorLabel.adjustsFontSizeToFitWidth = false
         authorLabel.lineBreakMode = .byTruncatingTail
-        
-        self.contentView.layer.cornerRadius = 8
-        
-        
+        authorLabel.textColor = UIColor.redditLightBlue
+        subredditLabel.textColor = UIColor.redditOrange
+        containerView.layer.cornerRadius = 10
+        containerView.layer.borderColor = UIColor.redditGrey.cgColor
+        containerView.layer.borderWidth = 1
+        containerView.layer.cornerRadius = 8
+        containerView.layoutMargins = UIEdgeInsets(top: 50, left: 50, bottom: 50, right: 50)
+        containerView.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
     }
     
-    //@IBOutlet var thumbnailView: UIImageView!
-    @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var subredditLabel: UILabel!
-    @IBOutlet var authorLabel: UILabel!
-    @IBOutlet var commentsLabel: UILabel!
-    @IBOutlet var dateLabel: UILabel!
-    @IBOutlet var thumbnailView: UIImageView!
-    
-   
+    @objc func contactUrlTapped(_ sender: UIButton)
+    {
+        guard let url = URL(string: "https://stackoverflow.com") else { return }
+        UIApplication.shared.open(url)
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        //thumbnailView.contentMode = .scaleAspectFit
-        // Initialization code
       
     }
-
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//        print("Seleccionada")
-//        // Configure the view for the selected state
-//    }
-    
 
 }
--- PopularPosts/ViewController.swift
+++ PopularPosts/ViewController.swift
@@ -13,37 +13,26 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     @IBOutlet var table: UITableView!
     @IBOutlet var prevButton: UIButton!
     @IBOutlet var nextButton: UIButton!
-    
-    
-
+    @IBOutlet var loadingIndicator:UIActivityIndicatorView!
     var resultsReddit:ResultsPosts?
     var rowCounter:Int = 0
     var isLoaded:Bool = false
-    
     var redditURL:String = "https://www.reddit.com/r/all/top.json"
-    
-   
-
-    
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       
-        print("Loading")
-        
         table.register(MyTableViewCell.nib(), forCellReuseIdentifier: MyTableViewCell.identifier)
         table.delegate = self
         table.dataSource = self
-        table.sectionFooterHeight = 10.0;
         nextButton.setTitle("\("next"~)", for: .normal)
         prevButton.setTitle("\("prev"~)", for: .normal)
-        table.backgroundColor = UIColor.redditLightestBlue
+        table.backgroundColor = UIColor.redditGrey
         table.layer.cornerRadius = 10
         table.rowHeight = UITableView.automaticDimension
-        
-        
-        //resultsReddit = self.parseJSON()
+        loadingIndicator.isHidden = false
+        table.isHidden = true
+        nextButton.isHidden = true
+        prevButton.isHidden = true
         
         AF.request(redditURL, method: .get).responseData {
             (response) in
@@ -55,49 +44,22 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 
                 do {
-                   // let dict: [String: Any] = getDictionarySomehow()
-                    
-
                     self.resultsReddit = try decoder.decode(ResultsPosts.self, from: value)
                     self.isLoaded = true
-                    //self.table. = 5
-                    //self
+                    self.loadingIndicator.isHidden = true
+                    self.table.isHidden = false
+                    self.nextButton.isHidden = false
+                    self.prevButton.isHidden = false
                     self.table.numberOfRows(inSection: 1)
                     self.table.reloadData()
-                    //self.resultsReddit = try decoder.decode(ResultsPosts.self,from:getData)
                 } catch  {
                     print("Invalid result \(error)")
                 }
-                
-                
-                
-                
             }else{
                 print("Not working")
             }
         }
         
-//         AF.request(redditURL).responseJSON {
-//            response in
-//
-//            print("Respuesta \(response)")
-//             self.isLoaded = true
-//
-////                do {
-////                    if case .success (let value) = response.result {
-////                        let decoder = JSONDecoder()
-////                        decoder.keyDecodingStrategy = .convertFromSnakeCase
-////                        result = try decoder.decode(ResultsPosts.self,from:response.data!)
-////                    }
-////                }catch{
-////                    print ("Error: \(error)")
-////                }
-//        }
-        
-    }
-    
-    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 10 // Espacio que deseas tener.
     }
     
 
@@ -109,39 +71,30 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         return 1
     }
     
-    
+    func tableView(_ tableView: UITableView, didSelectRowAtindexPath indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        
-        
-        //let customCell = Bundle.main.loadNibNamed("MyTableViewCell", owner: self, options: nil)?.first as! MyTableViewCell
-       
         let customCell = tableView.dequeueReusableCell(withIdentifier: "MyTableViewCell", for: indexPath) as! MyTableViewCell
 
-        customCell.layer.borderColor = UIColor.orange.cgColor
-        customCell.layer.borderWidth = 1
-        customCell.layer.cornerRadius = 8
-        customCell.layoutMargins = UIEdgeInsets(top: 50, left: 50, bottom: 50, right: 50)
-        customCell.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
-        customCell.separatorInset = UIEdgeInsets(top: 100, left: 100, bottom: 100, right: 100)
-                
+        customCell.backgroundColor = UIColor.redditGrey
         
         if (self.isLoaded){
             let totalRows = resultsReddit!.data.children.count
-            let row:Int = indexPath.section + rowCounter > totalRows ?  indexPath.section : indexPath.section + rowCounter
+            let row:Int = indexPath.section + rowCounter >= totalRows ?  indexPath.section : indexPath.section + rowCounter
             print("index row \(row)")
             
-            customCell.configure(with: resultsReddit!.data.children[row].data.title, subreddit: resultsReddit!.data.children[row].data.subredditNamePrefixed, author: resultsReddit!.data.children[row].data.author, comments: resultsReddit!.data.children[row].data.numComments, date: TimeInterval(resultsReddit!.data.children[row].data.created), thumbnailURL: resultsReddit!.data.children[row].data.thumbnail)
-            
-           
-
+            customCell.configure(with: resultsReddit!.data.children[row].data.title, subreddit: resultsReddit!.data.children[row].data.subredditNamePrefixed, author: resultsReddit!.data.children[row].data.author, comments: resultsReddit!.data.children[row].data.numComments, date: TimeInterval(resultsReddit!.data.children[row].data.created), thumbnailURL: resultsReddit!.data.children[row].data.thumbnail, auxURL: resultsReddit!.data.children[row].data.url)
         }
         return customCell
     }
     
     
     @IBAction func susCounter(sender:UIButton){
-        rowCounter -= 6
+       
         let alertController = UIAlertController(title: "\("alert_title"~)", message: "\("first_page"~)", preferredStyle: UIAlertController.Style.alert)
         
         
@@ -151,6 +104,8 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
             rowCounter = 0
             
             present(alertController,animated: true,completion: nil)
+        }else{
+            rowCounter -= 6
         }
         
         print("susCounter \(rowCounter)")
@@ -178,19 +133,13 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
    
     @IBAction func showAlert(sender: UIButton){
         
-        
         let buttonSender = sender
-        
         let wordToRead = buttonSender.titleLabel?.text
-        
         let alertController = UIAlertController(title: "Alerta titulo", message: wordToRead!, preferredStyle: UIAlertController.Style.alert)
-        
         alertController.addAction(UIAlertAction(title:"ok", style: UIAlertAction.Style.default, handler: nil))
         present(alertController,animated: true,completion: nil)
         
     }
-    
-    
     
     private func parseJSON() -> ResultsPosts?{
         var result: ResultsPosts?
@@ -198,32 +147,9 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
             return result
         }
         let url = URL(fileURLWithPath: path)
-        
-        
-//        await AF.request(redditURL).responseJSON {
-//            response in
-//            print("Respuesta \(response)")
-//
-////                do {
-////                    if case .success (let value) = response.result {
-////                        let decoder = JSONDecoder()
-////                        decoder.keyDecodingStrategy = .convertFromSnakeCase
-////                        result = try decoder.decode(ResultsPosts.self,from:response.data!)
-////                    }
-////                }catch{
-////                    print ("Error: \(error)")
-////                }
-//        }
+   
         
         do {
-            
-                
-                
-                
-//                .response { response in
-//                //debugPrint(response.response)
-//                jsonData = response.response
-//            }
             let jsonData = try Data(contentsOf: url)
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
@@ -239,9 +165,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         catch{
             print ("Error: \(error)")
         }
-            
-        //var jsonData:AnyObject
-       
         return result
     }
 }
